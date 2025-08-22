bce7fb80eda2325b3c11a5da5f0595f7d1bc204d
Lavi sharma <lavipandit123@gmail.com>
2025-08-22 21:59:44 +0530

    fixed backend url import

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ app.use(express.json());
 // app.use(cors());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://originhash-frontend.vercel.app"], 
+    origin: ["http://localhost:5173", "https://originhash.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // ✅ allow cookies + auth headers
